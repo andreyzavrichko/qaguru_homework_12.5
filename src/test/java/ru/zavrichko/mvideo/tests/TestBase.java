ff76723c570022e63756c4b597d490287972d8c2
Andrey Zavrichko <andrey@zavrichko.dev>
2022-02-07 16:22:51 +0200

    start project

---
+++ src/test/java/ru/zavrichko/mvideo/tests/TestBase.java
@@ -0,0 +1,37 @@
+package ru.zavrichko.mvideo.tests;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.junit5.AllureJunit5;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import ru.zavrichko.mvideo.config.Project;
+import ru.zavrichko.mvideo.helpers.AllureAttachments;
+import ru.zavrichko.mvideo.helpers.DriverSettings;
+import ru.zavrichko.mvideo.helpers.DriverUtils;
+
+
+@ExtendWith({AllureJunit5.class})
+public class TestBase {
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        DriverSettings.configure();
+    }
+
+    @AfterEach
+    public void addAttachments() {
+        String sessionId = DriverUtils.getSessionId();
+
+        AllureAttachments.addScreenshotAs("Last screenshot");
+        AllureAttachments.addPageSource();
+        AllureAttachments.addBrowserConsoleLogs();
+        Selenide.closeWebDriver();
+
+        if (Project.isVideoOn()) {
+            AllureAttachments.addVideo(sessionId);
+        }
+    }
+}
